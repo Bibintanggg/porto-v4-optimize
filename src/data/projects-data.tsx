8ee13d92025c0e8ab8abefe479ea166c1b5d703a
Bibintanggg <bintangyudhaa8@gmail.com>
2025-10-26 22:56:48 +0700

    feat: add new project

--- src/data/projects-data.tsx
+++ src/data/projects-data.tsx
@@ -1,4 +1,5 @@
 import { ProjectProps } from "@/types/project";
+import { DiJavascript } from "react-icons/di";
 import { FaLaravel, FaReact } from "react-icons/fa"
 import { IoLogoFirebase } from "react-icons/io5";
 import {
@@ -75,18 +76,60 @@ export const projectData: ProjectProps[] = [
         id: 4,
         title: "Lecturer Quiz App",
         description: "An employee management web app that provides features for logbook tracking, permit requests, and work monitoring.",
-        image: "/assets/projects/talent-force.png",
-        techStack: ['React', 'TailwindCSS', 'Typescript', 'Laravel Inertia', 'Laravel', 'ShadCN', 'Swagger'],
+        image: "/assets/projects/ucic.png",
+        techStack: ['TailwindCSS', 'JavaScript', 'Laravel'],
         link: "https://github.com/Bibintanggg",
         github: 'https://github.com/Bibintanggg',
         icon: [
+            <SiTailwindcss key={'tailwind'} size={20} />,
+            <DiJavascript key={'javascript'} size={20} />,
+            <FaLaravel key={'laravel'} size={20} />,
+        ]
+    },
+    {
+        id: 5,
+        title: "Jejak Pemuda",
+        description: "An employee management web app that provides features for logbook tracking, permit requests, and work monitoring.",
+        image: "/assets/projects/jejakpemuda.png",
+        techStack: ['TailwindCSS', 'React', 'Blender', 'ThreeJS'],
+        link: "https://github.com/Bibintanggg",
+        github: 'https://github.com/Bibintanggg',
+        icon: [
+            <SiTailwindcss key={'tailwind'} size={20} />,
             <FaReact key={'react'} size={20} />,
+            <SiBlender key={'blender'} size={20} />,
+            <SiThreedotjs key={'threejs'} size={20} />,
+        ]
+    },
+    {
+        id: 6,
+        title: "Kanban",
+        description: "An employee management web app that provides features for logbook tracking, permit requests, and work monitoring.",
+        image: "/assets/projects/kanban.png",
+        techStack: ['React', 'TailwinCSS', 'TypeScript', 'Laravel Inertia', 'Laravel', 'ShadCN'],
+        link: "https://github.com/Bibintanggg",
+        github: 'https://github.com/Bibintanggg',
+        icon: [
+             <FaReact key={'react'} size={20} />,
             <SiTailwindcss key={'tailwind'} size={20} />,
             <SiTypescript key={'typescript'} size={20} />,
             <SiInertia key={'inertia'} size={20} />,
             <FaLaravel key={'laravel'} size={20} />,
             <SiShadcnui key={'shadcn'} size={20} />,
-            <SiSwagger key={'swagger'} size={20}/>
+        ]
+    },
+    {
+        id: 7,
+        title: "Mini Finance Apps",
+        description: "An employee management web app that provides features for logbook tracking, permit requests, and work monitoring.",
+        image: "/assets/projects/finance.png",
+        techStack: ['TailwindCSS', 'JavaScript', 'Laravel'],
+        link: "https://github.com/Bibintanggg",
+        github: 'https://github.com/Bibintanggg',
+        icon: [
+            <SiTailwindcss key={'tailwind'} size={20} />,
+            <DiJavascript key={'javascript'} size={20} />,
+            <FaLaravel key={'laravel'} size={20} />,
         ]
     },
 ]
